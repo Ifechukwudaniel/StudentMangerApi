1c9337be1b15461c93370bc818042a087c6a44af
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-04-08 20:49:04 +0100

    back to normal

--- app/User/index.js
+++ app/User/index.js
@@ -8,34 +8,46 @@ const {
   Login,
   VerifyToken
 } = require("../../constants/routes")
-const {app} = require("../../server")
+const {
+  app
+} = require("../../server")
 const passport = require("passport")
-const {roleAuthorization} = require('../utils/roleAuthorization')
+const {
+  roleAuthorization
+} = require('../utils/roleAuthorization')
 
-  
+
 app.post(CreateUser,
-  passport.authenticate('jwt', {session:false, failureMessage:"Plese"}),
+  passport.authenticate('jwt', {
+    session: false
+  }),
   UserController.register
 );
 
 app.delete(DeleteById,
-  passport.authenticate('jwt', {session:false}),
+  passport.authenticate('jwt', {
+    session: false
+  }),
   roleAuthorization(['admin']),
   UserController.deleteUserById
 )
 
-app.get(FetchUserById, 
-  passport.authenticate('jwt', {session:false}),
+app.get(FetchUserById,
+  passport.authenticate('jwt', {
+    session: false
+  }),
   roleAuthorization(['admin']),
   UserController.getUserById
 )
 
-app.get(FetchUserAllUser, 
-  passport.authenticate('jwt', {session:false}),
+app.get(FetchUserAllUser,
+  passport.authenticate('jwt', {
+    session: false
+  }),
   roleAuthorization(['admin']),
   UserController.getUsers
 )
 
-app.post(Login , UserController.login(passport))
+app.post(Login, UserController.login(passport))
 
 app.post(VerifyToken, UserController.verifyToken)
