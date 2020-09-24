1ed8efbbc4332add12ea9b71accf781400e727d9
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-09-24 20:47:38 +0100

    added like and unlke for blog

--- app/Blog/controller.js
+++ app/Blog/controller.js
@@ -1,5 +1,7 @@
 const Blog= require("./model")
 const {missingParameterError, missingImageError}  = require("../utils/error")
+const { LikeBlogPost } = require("../../constants/routes")
+const _ = require('lodash')
 
 const  createBlog= (req,res, next)=>{
    const {
@@ -30,7 +32,7 @@ const  createBlog= (req,res, next)=>{
   })
 }
 
-getAllBlogs = (req, res, next )=>{
+ const getAllBlogs = (req, res, next )=>{
     Blog.find({})
     .populate({path:"comments",select:"time content user"})
     .limit(10)
@@ -39,7 +41,7 @@ getAllBlogs = (req, res, next )=>{
     })
 }
 
-deleteBlog = (req, res, next )=>{
+const deleteBlog = (req, res, next )=>{
     const {blogId} = req.params
     Blog.findByIdAndDelete(blogId)
     .then(()=>{
@@ -49,8 +51,64 @@ deleteBlog = (req, res, next )=>{
       return res.send({error:"Blog  could not be deleted"})
     })
 }
+
+
+const  likeBlogPost = (req, res,next)=>{
+   const {blog}  = req.params
+   Blog.findById(blog)
+   .then(data=>{
+     if(data.likedBy.includes(req.user.id)){
+      return res.send({message:"Blog was already liked by you"})
+     }
+     else{
+      Blog.update({"_id":data.id,},{
+        $push:{likedBy:req.user.id}
+      })
+      .then(()=>{
+       return res.send({message:"Like BLog"})
+      })
+     }
+   })
+   .catch(err=>{
+    return res.send({error:"The Blog was not found"})
+   })
+ }
+
+ const unLikeBlogPost = (userId)=>{
+
+ }
+
+ const unDisLikeBlogPost = (userId)=>{
+
+}
+
+ const  dislikeBlogPost = (req, res,next)=>{
+  const {blog}  = req.params
+  Blog.findById(blog)
+  .then(data=>{
+    console.log(data)
+    if(data.disLikedBy.includes(req.user.id)){
+     return res.send({message:"Blog was already disliked by you"})
+    }
+    else{
+     Blog.update({"_id":data.id,},{
+       $push:{disLikedBy:req.user.id}
+     })
+     .then(()=>{
+      return res.send({message:"Dislike BLog"})
+     })
+    }
+  })
+  .catch(err=>{
+    console.log(err)
+   return res.send({error:"The Blog was not found"})
+  })
+}
+
 module.exports = {
    createBlog,
    getAllBlogs,
-   deleteBlog
+   deleteBlog,
+   likeBlogPost,
+   dislikeBlogPost
 };
