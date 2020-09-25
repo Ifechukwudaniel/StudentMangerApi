a5501cb1e29a89ab2bc12edcfc7c99e213f0de4e
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-09-25 17:40:13 +0100

    creaate level and department

--- app/Departments/controller.js
+++ app/Departments/controller.js
@@ -1,18 +1,20 @@
 const Department = require('./model');
 const Level = require('../Levels/model')
+const {missingParameterError} = require('../utils/error');
+const {createLevelFromNumber} = require('../utils/levelCreate');
+
 const createDepartment =(req, res, next)=>{
   const {
    name
   } = req.body
 
   if(!name)
-  return  res.status(500).send(missingParameterError("department"))
+  return  res.status(500).send(missingParameterError("department name"))
   
     Department.find({name:name})
     .then((value)=>{
       if(value.length !==0){
-         console.log(value)
-        return res.status(500).json({error:"This department already exist"})
+        return res.status(500).json({message:"This department already exist"})
       }
       else{
         const department =  new Department({
@@ -21,7 +23,9 @@ const createDepartment =(req, res, next)=>{
        department
         .save()
         .then(department=>res.json(department))
-        .catch(err=> next(err))
+        .catch(err=>  {
+          return  res.status(500).json({message:"PLease an error occurred"})
+        })
       }
       
     })
@@ -29,11 +33,11 @@ const createDepartment =(req, res, next)=>{
 
 const getAllDepartment = (req, res, next)=>{
    Department.find({})
-  //  .select("-levels")
     .populate({path:'levels', select:'number'})
-    // .sort('descending')
    .then(departments=>res.json(departments))
-   .catch(err=>next(err))
+   .catch(err=>{
+      return  res.status(500).json({message:"PLease an error occurred"})
+   })
 }
 
 
@@ -48,6 +52,7 @@ const getDepartmentById= (department)=>{
   })
 
 })}
+
 const deleteDepartment = (req, res, next)=>{
   const {
     name
@@ -69,9 +74,55 @@ const deleteDepartment = (req, res, next)=>{
   })
 }
 
+
+const createDepartmentAndLevels= (req, res , next)=>{
+  const { name, numberOfLevels} = req.body
+
+
+if(!name)
+return  res.status(500).send(missingParameterError("department name"))
+
+if(!parseInt(numberOfLevels))
+return  res.status(500).send(missingParameterError("number of levels"))
+
+if(parseInt(numberOfLevels)> 8) return res.send(500).send({message:'The Number of level is to much'})
+
+  Department.find({name:name})
+  .then((value)=>{
+    if(value.length !==0){
+      return res.status(500).json({error:"This department already exist"})
+    }
+    else{
+         const department =  new Department({
+            name
+         })
+       const levels = createLevelFromNumber(parseInt(numberOfLevels), department._id) 
+       Level.insertMany(levels)
+       .then(data=>{
+         data.map(level=>{
+            department.levels.push(level._id)
+         })
+         department.save()
+         .then((department)=>{
+             return res.status(200).send({message:`Created department ${department.name} successfully`})
+         })
+       })
+       .catch(err=>{
+        return  res.status(500).json({message:"PLease an error occurred"})
+       })
+    }
+    
+  })
+  .catch(err=>{
+      return  res.status(500).json({message:"PLease an error occurred"})
+   })
+   
+}
+
   module.exports = {
     createDepartment,
     getAllDepartment,
     deleteDepartment,
-    getDepartmentById
+    getDepartmentById,
+    createDepartmentAndLevels
   };
