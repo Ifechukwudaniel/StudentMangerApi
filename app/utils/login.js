ec94022d270d053839003bbac57fd4eef6095664
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-09-26 23:35:58 +0100

    changes

--- app/utils/login.js
+++ app/utils/login.js
@@ -1,59 +1,87 @@
-const puppeteer = require('puppeteer')
-const uuid = require('crypto')
 const axios = require('axios')
-const xml2Json = require('xml2json') 
-
-// function logInApi (matricNumber, password){
-//     return new Promise((resolve, reject)=>{
-//         puppeteer
-//         .launch({headless:true}).then(async browser => {
-//         const page = await browser.newPage();
-//         await page.setRequestInterception(true);
-//         page.on('request', (req) => {
-//             if(req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image'){
-//                 req.abort();
-//             }
-//             else {
-//                 req.continue();
-//             }
-//         });
-//         await page.goto('https://www.biuportal.net/LogIn.aspx#', {waitUntil: 'load', timeout: 100000})
-//         await page.click('a#modalCloseBtn');
-//         await page.type('#txtUserName', matricNumber);
-//         await page.type("#txtPassword", password);
-//         await page.click("#btnLogin")
-//         await page.waitForSelector("span#lblErrorMessage")
-//         .then(async ()=>{
-//             const error = await page.$eval("span#lblErrorMessage", el=>el.innerHTML)
-//             reject( {error:error})
-//         })
-//         .catch( async (err)=>{
-//           const name = await page.$eval("span#lblMasterStudentsName", el=>el.innerHTML)
-//           resolve({name:name,matricNumber:matricNumber, password:password})
-//         })
-//       })
-//       .catch( err=>{
-//         reject( {error:"Please an error occurred "})
-//       })
-
-// })}
+const  qs = require('qs');
+var HTMLParser = require('node-html-parser');
 
 const logInApi = ( matricNumber, password)=>{
     return new Promise((resolve, reject)=>{
-        axios.get(`https://www.biuportal.net/Services/CbtClientRequests.aspx?OPERATION=SIGNIN&USER_NAME=${matricNumber}&PASSWORD=${password}&VERSION=NEW-VERSION`)
-        .then(({data})=>{
-          let userData =JSON.parse(xml2Json.toJson(data))
-           if(userData.NewDataSet.Table1.RESPONSE=="SUCCESSFUL"){
-              resolve(userData.NewDataSet.Table1)
-           }
-           else{
-            reject({
-                RESPONSE:"FAILED",
-                MESSAGE:"CHECK YOUR USER AND PASSWORD"
-            })
-           }
+        var data = qs.stringify({
+         '__VIEWSTATE': '/wEPDwUJNTMzNDU1NzA1D2QWAgIDDxYCHgxhdXRvY29tcGxldGUFA29mZmRkUqof2LAdE4XaTKVxV+vLr66d+Qk=',
+        '__VIEWSTATEGENERATOR': 'C2EE9ABB',
+        '__EVENTVALIDATION': '/wEdAARd9vtBLyjpHiBYSoOme+1hY3plgk0YBAefRz3MyBlTcHY2+Mc6SrnAqio3oCKbxYainihG6d/Xh3PZm3b5AoMQ9QuH9fLNL5w33RiP/YoinnH+MVI=',
+        'txtUserName': matricNumber,
+        'txtPassword': password,
+        'btnLogin': 'Login' 
+        });
+        var config = {
+          method: 'post',
+          url: 'https://www.biuportal.net/LogIn.aspx',
+          headers: { 
+            'authority': 'www.biuportal.net', 
+            'pragma': 'no-cache', 
+            'cache-control': 'no-cache', 
+            'upgrade-insecure-requests': '1', 
+            'origin': 'https://www.biuportal.net', 
+            'content-type': 'application/x-www-form-urlencoded', 
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36', 
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 
+            'sec-fetch-site': 'same-origin', 
+            'sec-fetch-mode': 'navigate', 
+            'sec-fetch-user': '?1', 
+            'sec-fetch-dest': 'document', 
+            'referer': 'https://www.biuportal.net/LogIn.aspx', 
+            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8', 
+            'cookie': 'ASP.NET_SessionId=zllvytfxj4tcdfgqot31ei0a'
+          },
+          data : data
+        };
+        
+        let userImg= ''
+        
+        axios(config)
+        .then(function (response) {
+          const root = HTMLParser.parse(response.data);
+          if(root.querySelector("#lblErrorMessage")){
+            reject({  Response:"FAILED",Message:root.querySelector("#lblErrorMessage").innerHTML})
+         }
+          else{
+              userImg =root.querySelector("#imgPassport").getAttribute('src')
+              var config = {
+                method: 'get',
+                url: 'https://www.biuportal.net/Students/Results/Results.aspx',
+                headers: { 
+                  'authority': 'www.biuportal.net', 
+                  'pragma': 'no-cache', 
+                  'cache-control': 'no-cache', 
+                  'upgrade-insecure-requests': '1', 
+                  'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36', 
+                  'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 
+                  'sec-fetch-site': 'same-origin', 
+                  'sec-fetch-mode': 'navigate', 
+                  'sec-fetch-user': '?1', 
+                  'sec-fetch-dest': 'document', 
+                  'referer': 'https://www.biuportal.net/LogIn.aspx', 
+                  'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8', 
+                  'cookie': 'ASP.NET_SessionId=zllvytfxj4tcdfgqot31ei0a; ASP.NET_SessionId=giswpev3skkc0fqibabrr0vo'
+                }
+              };
+              
+              axios(config)
+              .then(function (response) {
+                const newData = HTMLParser.parse(response.data);
+                const fullName = newData.querySelector("#HeaderContent_lblFullName").innerHTML
+                const department = newData.querySelector("#HeaderContent_lblDepartment").innerHTML
+                const level =parseInt(newData.querySelector("#HeaderContent_lblLevel").innerHTML)
+                 resolve({ Response:"SUCCESS",image:userImg,fullName, department,level})
+              })
+              .catch(function (error) {
+                reject({Response:"FAILED",Message:"Please an error occurred"})
+              });
+          }
         })
-    })
+        .catch(function (error) {
+          reject({Response:"FAILED",Message:"Please an error occurred"})
+        });
+        }) 
 }
 
 const authorization = () =>{
--- slo.js
+++ slo.js
@@ -1,12 +1,19 @@
 const axios = require('axios');
 const  qs = require('qs');
+const firebase = require('./firebase')
 var HTMLParser = require('node-html-parser');
+const uuid = require('uuid')
+const filePath = `image/users/${uuid.v4()}.jpg`
+
+
+const signUser=(matricNumber, password)=>{
+  return new Promise((resolve, reject)=>{
 var data = qs.stringify({
  '__VIEWSTATE': '/wEPDwUJNTMzNDU1NzA1D2QWAgIDDxYCHgxhdXRvY29tcGxldGUFA29mZmRkUqof2LAdE4XaTKVxV+vLr66d+Qk=',
 '__VIEWSTATEGENERATOR': 'C2EE9ABB',
 '__EVENTVALIDATION': '/wEdAARd9vtBLyjpHiBYSoOme+1hY3plgk0YBAefRz3MyBlTcHY2+Mc6SrnAqio3oCKbxYainihG6d/Xh3PZm3b5AoMQ9QuH9fLNL5w33RiP/YoinnH+MVI=',
-'txtUserName': 'bas/csc/170387',
-'txtPassword': 'password1',
+'txtUserName': matricNumber,
+'txtPassword': password,
 'btnLogin': 'Login' 
 });
 var config = {
@@ -32,13 +39,59 @@ var config = {
   data : data
 };
 
+let userImg= ''
+
 axios(config)
 .then(function (response) {
   const root = HTMLParser.parse(response.data);
-  // console.log(root)
-   console.log(root.querySelector("#lblErrorMessage").innerHTML)
-  
+  if(root.querySelector("#lblErrorMessage")){
+    reject({  Response:"FAILED",Message:root.querySelector("#lblErrorMessage").innerHTML})
+ }
+  else{
+      userImg =root.querySelector("#imgPassport").getAttribute('src')
+      var config = {
+        method: 'get',
+        url: 'https://www.biuportal.net/Students/Results/Results.aspx',
+        headers: { 
+          'authority': 'www.biuportal.net', 
+          'pragma': 'no-cache', 
+          'cache-control': 'no-cache', 
+          'upgrade-insecure-requests': '1', 
+          'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36', 
+          'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 
+          'sec-fetch-site': 'same-origin', 
+          'sec-fetch-mode': 'navigate', 
+          'sec-fetch-user': '?1', 
+          'sec-fetch-dest': 'document', 
+          'referer': 'https://www.biuportal.net/LogIn.aspx', 
+          'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8', 
+          'cookie': 'ASP.NET_SessionId=zllvytfxj4tcdfgqot31ei0a; ASP.NET_SessionId=giswpev3skkc0fqibabrr0vo'
+        }
+      };
+      
+      axios(config)
+      .then(function (response) {
+        const newData = HTMLParser.parse(response.data);
+        const fullName = newData.querySelector("#HeaderContent_lblFullName").innerHTML
+        const department = newData.querySelector("#HeaderContent_lblDepartment").innerHTML
+        const level =parseInt(newData.querySelector("#HeaderContent_lblLevel").innerHTML)
+         resolve({ Response:"SUCCESS",image:userImg,fullName, department,level})
+      })
+      .catch(function (error) {
+        reject({Response:"FAILED",Message:"Please an error occurred"})
+      });
+  }
 })
 .catch(function (error) {
-  console.log(error);
+  reject({Response:"FAILED",Message:"Please an error occurred"})
 });
+})
+}
+
+signUser()
+.then(data=>{
+  console.log(data)
+})
+.catch(err=>{
+  console.log(err)
+})
